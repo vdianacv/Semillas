0c2751c3f9932295963faca0a94b0c59c3d98248
Diana Valencia <vdiana1895@gmail.com>
2022-03-11 13:19:46 -0700

    commit on master

--- js/app.js
+++
@@ -1,6 +0,0 @@
-+function cambiarclase(){
-  let sidebarnav = document.getElementsByClassName('sidebar-nav');
-      sidebarnav.classlist.toggle('sidebar-nav-open');
-  let menuopen = document.getElementsByClassName('menu-toggle');
-      menuopen.classlist.toggle('menu-open');
-}
